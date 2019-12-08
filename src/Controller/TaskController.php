d41c9d25721e7adcdbb32f4e69a47ec14be1ed1b
Vlad Bogdan <vlad.bogdan90@gmail.com>
2019-12-08 20:41:32 +0100

    add controller

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('todo');
+    }
+
+    /**
+     * @Route("/todo", name="todo")
+     */
+    public function todo(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to todo api',
+        ]);
+    }
+
+    /**
+     * @Route("todo/list", name="todo_list")
+     */
+    public function list(): JsonResponse
+    {
+        $taskList = $this->getDoctrine()
+            ->getRepository(Task::class)
+            ->findAll();
+
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonData = $serializer->serialize($taskList, 'json');
+        $data = json_decode($jsonData);
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("todo/create", name="todo_create")
+     */
+    public function create(): JsonResponse
+    {
+        return $this->json([
+            'message' => '/todo/create',
+        ]);
+    }
+
+    /**
+     * @Route("todo/done", name="todo_done")
+     */
+    public function done(): JsonResponse
+    {
+        return $this->json([
+            'message' => '/todo/done',
+        ]);
+    }
+
+
+    /**
+     * @Route("todo/delete", name="todo_delete")
+     */
+    public function delete(): JsonResponse
+    {
+        return $this->json([
+            'message' => '/todo/delete',
+        ]);
+    }
+}
